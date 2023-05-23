44c1faf7ca2bfe163533dccb89ebac898fd61555
yara hammami <yara.hammami13@gmail.com>
2023-05-23 15:42:20 +0300

    added the aggregation task

--- dag.py
+++ dag.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from pre_processing import pre_process
-
+from pre_process import process_data
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2023, 5, 18)
@@ -21,6 +21,11 @@ with DAG(dag_id="workflow", default_args=default_args, schedule_interval='@daily
         python_callable=pre_process,
     )
 
+    aggregation = PythonOperator(
+        task_id="aggregation",
+        python_callable=process_data,
+    )
 
-check_file >> pre_process
+
+check_file >> pre_process >> aggregation
 #docker-compose -f .\docker-compose-LocalExecutor.yml down
